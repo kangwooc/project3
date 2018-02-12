7603e74022434beeb0d7dac8c9a5de58e9dce284
Demi Tu <demigod@uw.edu>
2018-02-12 15:41:58 -0800

    Made changes to ArrayHeap and TestArrayHeapFunctionality

--- project3/src/main/java/datastructures/concrete/ArrayHeap.java
+++ project3/src/main/java/datastructures/concrete/ArrayHeap.java
@@ -1,6 +1,7 @@
 package datastructures.concrete;
 
 import datastructures.interfaces.IPriorityQueue;
+import misc.exceptions.EmptyContainerException;
 import misc.exceptions.NotYetImplementedException;
 
 /**
@@ -16,9 +17,11 @@ public class ArrayHeap<T extends Comparable<T>> implements IPriorityQueue<T> {
     private T[] heap;
 
     // Feel free to add more fields and constants.
+    private int size;
 
     public ArrayHeap() {
-        throw new NotYetImplementedException();
+        this.heap = this.makeArrayOfT(21);
+        this.size = 0;
     }
 
     /**
@@ -36,24 +39,55 @@ public class ArrayHeap<T extends Comparable<T>> implements IPriorityQueue<T> {
         // works, and should not modify it in any way.
         return (T[]) (new Comparable[size]);
     }
-
+    
+    // empty or one element afterwards don't need to do anything
     @Override
     public T removeMin() {
-        throw new NotYetImplementedException();
+        if (this.size == 0) {
+            throw new EmptyContainerException();
+        }
+        T min = this.heap[0];
+        this.heap[0] = this.heap[this.size - 1];
+        this.heap[this.size - 1] = null;
+        this.size--;
+        if (this.size > 1) {
+            percolateDown(0);
+        }
+        return min;
     }
 
+    // null children?
+    // base case?
+    private void percolateDown(int index) {
+        int newIndex = index;
+        int min = 10000;
+        for (int j = 1; j <= NUM_CHILDREN; j++) {
+            int childIndex = NUM_CHILDREN * index + j;
+            int difference = this.heap[index].compareTo(this.heap[childIndex]);
+            if (difference < min) {
+                newIndex = childIndex;
+            }
+        }
+        percolateDown(newIndex);
+    }
+    
     @Override
     public T peekMin() {
-        throw new NotYetImplementedException();
+        if (this.size == 0) {
+            throw new EmptyContainerException();
+        }
+        return this.heap[0];
     }
 
+    // percolate up
     @Override
     public void insert(T item) {
+        // T parent = Math.floor((index - 1) / NUM_CHILDREN);
         throw new NotYetImplementedException();
     }
 
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return this.size;
     }
 }
--- project3/src/test/java/datastructures/sorting/TestArrayHeapFunctionality.java
+++ project3/src/test/java/datastructures/sorting/TestArrayHeapFunctionality.java
@@ -1,8 +1,12 @@
 package datastructures.sorting;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Random;
 
 import misc.BaseTest;
+import misc.exceptions.EmptyContainerException;
 import datastructures.concrete.ArrayHeap;
 import datastructures.interfaces.IPriorityQueue;
 import org.junit.Test;
@@ -20,6 +24,127 @@ public class TestArrayHeapFunctionality extends BaseTest {
         IPriorityQueue<Integer> heap = this.makeInstance();
         heap.insert(3);
         assertEquals(1, heap.size());
+        assertTrue(!heap.isEmpty());
+    }
+    
+    // new tests below
+    @Test(timeout = SECOND)
+    public void testInsertandremoveMinBasic() {
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        for (int i = 0; i < 3; i++) {
+            heap.insert(i);
+        }
+        assertEquals(0, heap.removeMin());
+        assertEquals(1, heap.removeMin());
+        assertEquals(2, heap.removeMin());
+        assertTrue(heap.isEmpty());
+    }
+
+    @Test(timeout = SECOND)
+    public void testInsertAndremoveMinReversely() {
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        for (int i = 100; i >= 0; i--) {
+            heap.insert(i);
+        }
+        assertEquals(0, heap.peekMin());
+        assertEquals(101, heap.size());
+        for (int i = 0; i < heap.size(); i++) {
+            heap.removeMin();
+        }
+        assertTrue(heap.isEmpty());
+    }
+
+    @Test(timeout = SECOND)
+    public void testRemoveMinErrorHandling() {
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        try {
+            heap.removeMin();
+            fail("Expected EmptyContainerException");
+        } catch (EmptyContainerException ex) {
+
+        }
+        heap.insert(3);
+        heap.removeMin();
+        try {
+            heap.removeMin();
+            fail("Expected EmptyContainerException");
+        } catch (EmptyContainerException ex) {
+
+        }
+        assertEquals(1, heap.size());
+        assertTrue(!heap.isEmpty());
+    }
+
+    @Test(timeout = SECOND)
+    public void testInsertErrorHandling() {
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        try {
+            heap.insert(null);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException ex) {
+
+        }
+        for (int i = 100; i >= 0; i--) {
+            heap.insert(i);
+        }
+        try {
+            heap.insert(null);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException ex) {
+
+        }
+    }
+
+    @Test(timeout = SECOND)
+    public void testpeekMinBasic() {
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        for (int i = 0; i < 3; i++) {
+            heap.insert(i);
+        }
+        assertEquals(0, heap.peekMin());
+        assertEquals(3, heap.size());
+        assertTrue(!heap.isEmpty());
+    }
+
+    @Test(timeout = SECOND)
+    public void testpeekMinErrorHandling() {
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        try {
+            heap.peekMin();
+            fail("Expected EmptyContainerException");
+        } catch (EmptyContainerException ex) {
+
+        }
+    }
+
+    @Test(timeout = SECOND)
+    public void testpeekMinAndInsert() {
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        for (int i = -5; i < 5; i++) {
+            heap.insert(i);
+        }
+        assertEquals(-5, heap.peekMin());
+        assertEquals(10, heap.size());
+    }
+
+    @Test(timeout = SECOND)
+    public void testpeekMinAndInsertReversely() {
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        for (int i = 100; i >= 0; i--) {
+            heap.insert(i);
+        }
+        assertEquals(0, heap.peekMin());
+        assertEquals(101,heap.size());
+    }
+    
+    @Test(timeout = SECOND)
+    public void testInsertRandomly() {
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        Random rand = new Random(); 
+        for (int i = 0; i < 100; i++) {
+            int randomNum = rand.nextInt(100);
+            heap.insert(randomNum);
+        }
         assertTrue(!heap.isEmpty());
     }
 }
